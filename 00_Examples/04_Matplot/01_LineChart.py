ecf3ef0e9f331c899ec6ea1b37f229149af78d9b
Herreria <LHerreria@ad.harman.com>
2017-10-16 13:23:05 -0500

    Added a new folder for the matplot library example and included the first example, which is a line chart

---
+++ 00_Examples/04_Matplot/01_LineChart.py
@@ -0,0 +1,20 @@
+"""
+	Author: Luis_C-137
+	Using joins exmaple
+	This is just for practice purposes
+	This is NOT functional code
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.linspace(0,10,100) # Start point, end point, number of points
+
+y = np.sin(x)
+
+plt.plot(x,y)
+
+plt.xlabel("Time")
+plt.ylabel("Function of time")
+plt.title("My first chart")
+
+plt.show();
