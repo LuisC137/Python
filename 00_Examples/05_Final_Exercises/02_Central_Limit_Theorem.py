d865a34d3df19ea2ee5b9bf64dd3197d4da32703
Herreria <LHerreria@ad.harman.com>
2017-10-16 16:26:26 -0500

    Another simple example

---
+++ 00_Examples/05_Final_Exercises/02_Central_Limit_Theorem.py
@@ -0,0 +1,14 @@
+"""
+	Author: Luis_C-137
+	Demostrate the central limit theorem
+	This is just for practice purposes
+	This is NOT functional code
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+X = np.random.random(10000)
+
+plt.hist(X)
+plt.show()
